12eb68b6ac63b497970299399fc73c488103db0c
jax.mossman@gmail.com <jax.mossman@gmail.com>
2019-09-11 17:55:41 -0500

    Added notes

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -54,7 +54,7 @@ namespace NumberGuessingGame
                     correct = true;
                 }
                 //user lies
-                else if (max == min)
+                else if (max == min )
                 {
                     Console.WriteLine("i know what you did");
                     failcount++;
@@ -72,55 +72,55 @@ namespace NumberGuessingGame
                         Console.WriteLine("they look at me like they dont even know me anymore");
                     }
                 }
-
             }
             Console.WriteLine("your number is " + guess + " Loser");
             Console.ReadKey();
-
         }
-        //void Game()
-        //{
-        //    int number = 0;
-        //    int guess = 0;
-        //    int max = 11;
-        //    int min = 0;
-        //    Random random = new Random();
-        //    Console.WriteLine("Please choose a number 1-10");
-        //    number = Convert.ToInt32(Console.ReadLine());
-        //    Convert.ToInt32(number);
+        
+//|Jackson Mossman|
+//|:---           |
+//|s198037|
+//|Number Guessing Game Assessment|
+//|Goal:To guess the users number through random guessing and elimination|
 
-        //    if (number < 0 || number > 10)
-        //    {
-        //        Console.WriteLine("Invalid Number");
-        //    }
-        //    while (guess != number)
-        //    {
-        //        string choice = "";
-        //        Console.WriteLine("the guess is " + guess);
-        //        Console.WriteLine("Is the number greater or less than the guess");
-        //        Console.WriteLine("1: guess > number");
-        //        Console.WriteLine("2: guess < number");
-        //        choice = Console.ReadLine();
-        //        if (choice == "1" && guess > number)
-        //        {
-        //            guess = random.Next(min, guess + 1);
-        //        }
-        //        //guess is to low
-        //        else if (choice == "2" && guess < number)
-        //        {
-        //            guess = random.Next(guess + 1, max);
-        //        }
-        //        //user lies
-        //        else
-        //        {
-        //            Console.WriteLine("i know what you did");
-        //            guess = random.Next(min, max);
-        //        }
+//**Inputs Info:
+//User will input minimum Number and maximum Number
+//User will choose if the guess is higher lower or corrrect
+//**User must hold number in thier head
 
-        //    }
-        //    Console.WriteLine("your number is " + guess + " Loser");
-        //    Console.ReadKey();
-        //}
+//#Output Info:
+//Outputs random numbers everytime users gives input of higher and lower
+
+
+
+//##System Architecture
+//Contained in only Program.Cs
+//Runs in a single loop of the game then ends the game
+
+//##Program.Cs
+
+
+//Name:Min(int)
+//Description:holds the minimum value for Guessing
+
+//Name:Max(int)
+//Description:Holds a maximum value for Guessing
+
+//Name:Guess(int)
+//Description:Holds value of current Guess used to overwrite min and max values
+
+//Name:fail_count(int)
+//Description:holds how many times they have made a invalid range
+
+//Name:Correct(bool)
+//Description:Breaks while loop when set to true
+
+
+
+//#--Know Bugs
+//- will sometime Output the same value previous guess
+
+
 
     }
 }
